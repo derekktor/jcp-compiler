7b891c6db1a6132f474ecc3da5c0325b660a68ec
sn3aky <konradlab01@gmail.com>
2022-06-16 18:56:41 +0200

    tabs

--- Listener.py
+++ Listener.py
@@ -4,36 +4,48 @@ from utils.jcpParser import jcpParser
 class Listener(jcpListener):
     def __init__(self, output):
         self.output = output
-        self.output.write('#include <iostream>\n')
+        self.output.write('#include <iostream>\n\n')
+        self.tab = 0
+        self.nl = False
+    
+    def write(self, text):
+        if(self.nl):
+            for i in range(self.tab):
+                self.output.write('\t')
+        self.output.write(text)
+        self.nl = text.endswith('\n')
 
     def enterImportDec(self, ctx: jcpParser.ImportDecContext):
-        self.output.write("#include <" + ctx.getChild(1).getText() + ">\n")
+        self.write("#include <" + ctx.getChild(1).getText() + ">\n")
 
     def enterClassDec(self, ctx: jcpParser.ClassDecContext):
-        self.output.write(ctx.CLASS().getText() + ' ' + ctx.IDENTIFIER().getText())
+        self.write(ctx.CLASS().getText() + ' ' + ctx.IDENTIFIER().getText())
 
     def exitClassDec(self, ctx: jcpParser.ClassDecContext):
-        self.output.write('};\n')
+        self.write('};\n')
     
     def enterExtendsDec(self, ctx: jcpParser.ExtendsDecContext):
-        self.output.write(' : ' + ctx.IDENTIFIER().getText())
+        self.write(' : ' + ctx.IDENTIFIER().getText())
 
     def enterBody(self, ctx: jcpParser.BodyContext):
-        self.output.write(' {\n')
-
+        self.write(' {\n')
+        self.tab+=1
+    
+    def exitBody(self, ctx):
+        self.tab-=1
 
     def enterMemberDec(self, ctx: jcpParser.MemberDecContext):
-        self.output.write(ctx.modifier().getText()+':\n')
-        self.output.write(ctx.type_().getText()+' '+ctx.IDENTIFIER().getText()+';\n')
+        self.write(ctx.modifier().getText()+':\n')
+        self.write(ctx.type_().getText()+' '+ctx.IDENTIFIER().getText()+';\n')
 
     def enterLocalDec(self, ctx: jcpParser.LocalDecContext):
-        self.output.write(ctx.type_().getText()+' '+ctx.IDENTIFIER().getText())
+        self.write(ctx.type_().getText()+' '+ctx.IDENTIFIER().getText())
 
     def enterAssign(self, ctx: jcpParser.AssignContext):
-        self.output.write(' = '+ctx.rightHandSide().getText())
+        self.write(' = '+ctx.rightHandSide().getText())
     
     def exitLocalDec(self, ctx: jcpParser.LocalDecContext):
-        self.output.write(';\n')
+        self.write(';\n')
 
     def getTypeText(self, ctx: jcpParser.TypeContext):
         if(ctx.getText()=='String'):
@@ -43,40 +55,42 @@ class Listener(jcpListener):
 
     def enterMethodDec(self, ctx: jcpParser.MethodDecContext):
         if ctx.modifier() is not None:
-            self.output.write(ctx.modifier().getText()+ ':\n')
+            self.write(ctx.modifier().getText()+ ':\n')
         if ctx.STATIC() is not None:
-            self.output.write(ctx.STATIC().getText()+ ' ')
-        self.output.write(self.getTypeText(ctx.type_())+' '+ctx.IDENTIFIER().getText())
+            self.write(ctx.STATIC().getText()+ ' ')
+        self.write(self.getTypeText(ctx.type_())+' '+ctx.IDENTIFIER().getText())
+        self.tab+=1
 
     def printParameter(self, parameter):
-        self.output.write(self.getTypeText(parameter.type_()))
-        self.output.write(' '+parameter.IDENTIFIER().getText())
-        if parameter.LBRACK() is not None: self.output.write('[')
-        if parameter.RBRACK() is not None: self.output.write(']')
+        self.write(self.getTypeText(parameter.type_()))
+        self.write(' '+parameter.IDENTIFIER().getText())
+        if parameter.LBRACK() is not None: self.write('[')
+        if parameter.RBRACK() is not None: self.write(']')
 
     def enterParameters(self, ctx: jcpParser.ParametersContext):
-        self.output.write('(')
+        self.write('(')
         for i in range(0, len(ctx.parameter())):
             self.printParameter(ctx.parameter(i))
             if i != len(ctx.parameter()) - 1:
-                self.output.write(', ')
-        self.output.write(') {\n')
+                self.write(', ')
+        self.write(') {\n')
 
 
     def exitMethodDec(self, ctx: jcpParser.MethodDecContext):
-        self.output.write('}\n')
+        self.tab-=1
+        self.write('}\n')
     
     # def enterStatement(self, ctx: jcpParser.StatementContext):
-    #     self.output.write(ctx.getText())
-    #     self.output.write('\n')
+    #     self.write(ctx.getText())
+    #     self.write('\n')
 
     def enterReturnStatement(self, ctx: jcpParser.ReturnStatementContext):
-        self.output.write('return '+ctx.expression().getText()+';\n')  
+        self.write('return '+ctx.expression().getText()+';\n')  
 
     def enterSout(self, ctx: jcpParser.SoutContext):
-        self.output.write('std::cout << '+ctx.expression().getText()+' << std::endl;\n')
+        self.write('std::cout << '+ctx.expression().getText()+' << std::endl;\n')
 
     def exitStart(self, ctx: jcpParser.StartContext):
-        self.output.write('\nint main() {\n')
-        self.output.write('\tMain::main({});\n')
-        self.output.write('\treturn 0;\n}\n')
+        self.write('\nint main() {\n')
+        self.write('\tMain::main({});\n')
+        self.write('\treturn 0;\n}\n')
